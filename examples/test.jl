d7d024bbf46aafc8140df44dda912d46c8ea97ac
Marco Artiano <artianomarco97@gmail.com>
2025-02-18 13:53:13 +0100

    minor changes

--- examples/test.jl
+++ examples/test.jl
@@ -1,6 +1,9 @@
 using IncompressibleNavierStokes
 
-Nx = 10
-Nz = 10
+nx = 10
+nz = 10
 domain = (0.0, 1.0, 0.0, 2.0)
-grid = mesh(domain, Nx, Nz)
+grid = mesh(domain, nx, nz)
+
+semi = SemiDiscretization(grid, nothing, nothing, nothing)
+
